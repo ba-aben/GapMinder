7e9aa750e08a10d9c20bfd914a60e87ddc4f4eca
ba-aben <ba.aben@gmail.com>
2020-07-27 16:26:36 +0200

    Moved Code.R to /scripts

---
+++ scripts/Code.R
@@ -0,0 +1,6 @@
+data <- read.csv(file = 'data//gapminder_data.csv')
+data <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv")
+dim(data)
+str(data)
+head(data)
+
